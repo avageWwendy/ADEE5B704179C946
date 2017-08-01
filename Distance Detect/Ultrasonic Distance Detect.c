2c73fee9e562625350fccf42f96b75816794d4d3
avageWwendy <avage.Wwendy@sjtu.edu.cn>
2017-08-01 13:16:49 +0800

    update Distance Detect test

--- Distance Detect/Ultrasonic Distance Detect.c
+++ Distance Detect/Ultrasonic Distance Detect.c
@@ -1,16 +1,15 @@
 #include <p32xxxx.h>
 #include <plib.h>
 
-#define Ultrasonic1_ECHO	PORTDbits.RD8
-#define Ultrasonic1_TRIG	PORTDbits.RD9
-#define Ultrasonic2_ECHO	PORTDbits.RD10
-#define Ultrasonic2_TRIG	PORTDbits.RD11
+#define Ultrasonic1_TRIG	PORTDbits.RD8
+#define Ultrasonic1_ECHO	PORTDbits.RD9
+#define Ultrasonic2_TRIG	PORTDbits.RD10
+#define Ultrasonic2_ECHO	PORTDbits.RD11
+
 
 float distance1 = 0.0;		// unit: m
 float distance2 = 0.0;		// unit: m
 unsigned char detector = 0x1;	// choose which detector
-short IC1begin = 0;				// flag for rising edge(0) / falling edge(1)
-short IC3begin = 0;				// flag for rising edge(0) / falling edge(1)
 
 void Config_Timer5() {
 	IPC5SET = 0x0000000C;	// Set priority level = 3
@@ -29,27 +28,31 @@ void Config_Timer5() {
 #pragma interrupt T5_ISR ipl3 vector 20
 void T5_ISR() {
 	if (detector == 0x1){
-		Ultrasonic1_TRIG = 1;	// Generate 10 us pulse signal to trigger Ultrasonic Distance Detector 1
+		Ultrasonic1_ECHO = 0;
+		Ultrasonic1_TRIG = 1;	// Generate 15 us pulse signal to trigger Ultrasonic Distance Detector 1
 		unsigned int tStart = ReadCoreTimer();
-		while((ReadCoreTimer()-tStart)<40);
+		while((ReadCoreTimer()-tStart)<60);
 		Ultrasonic1_TRIG = 0;
+		IC2CONSET = 0x8000;		// start Input Capture 2 (IC2)
 		detector = 0x2;
 	}
 	else if (detector == 0x2){
-		Ultrasonic2_TRIG = 1;	// Generate 10 us pulse signal to trigger Ultrasonic Distance Detector 2
+		Ultrasonic2_ECHO = 0;
+		Ultrasonic2_TRIG = 1;	// Generate 15 us pulse signal to trigger Ultrasonic Distance Detector 2
 		unsigned int tStart = ReadCoreTimer();
-		while((ReadCoreTimer()-tStart)<40);
+		while((ReadCoreTimer()-tStart)<60);
 		Ultrasonic2_TRIG = 0;
+		IC4CONSET = 0x8000;		// start Input Capture 4 (IC4)
 		detector == 0x1;
-	}
+	} 
 	IFS0CLR = 0x00100000;	// Clear Timer 5 interrupt flag
 }
 
 void Config_UltrasonicDistDetectors(){
-	PORTDCLR = 0x0F00;	// clear RD[8], RD[9], RD[10], RD[11]
-	TRISDCLR = 0x0F00;	// set RD[8] = TRIG1
-						// set RD[9] = ECHO1
+	LATDCLR = 0x0F00;	// clear RD[8], RD[9], RD[10], RD[11]
+	TRISDSET = 0x0A00;	// set RD[8] = TRIG1
 						// set RD[10] = TRIG2
+	TRISDCLR = 0x0500;	// set RD[9] = ECHO1
 						// set RD[11] = ECHO2
 }
 
@@ -64,49 +67,59 @@ void Config_Timer3() {
 }
 
 void Config_ICs(){
-	/** Config **/
-	IPC1SET = 0x00001000;	// Set priority level = 4
-	IPC1SET = 0x00000100;	// Set subpriority level = 1
-	IFS0CLR = 0x00000020;	// Clear IC1 interrupt flag
-	IEC0SET = 0x00000020;	// Enable IC1 interrupts
-	IC1CON = 0x00000206;	// Disable and reset the IC Module, disable interrupt generation and allow SFR modifications;
+	/** IC2/RD9 **/
+	IPC2SET = 0x00001000;	// Set priority level = 4
+	IPC2SET = 0x00000100;	// Set subpriority level = 1
+	IFS0CLR = 0x00000200;	// Clear IC2 interrupt flag
+	IEC0SET = 0x00000200;	// Enable IC2 interrupts
+	IC2CON = 0x00000206;	// Disable and reset the IC Module, disable interrupt generation and allow SFR modifications;
 							// simple Capture Event mode, capture rising edge first, count source is 16-bit Timer3
 							// interrupt on every capture event
-	IPC3SET = 0x00001000;	// Set priority level = 4
-	IPC3SET = 0x00000100;	// Set subpriority level = 1
-	IFS0CLR = 0x00002000;	// Clear IC3 interrupt flag
-	IEC0SET = 0x00002000;	// Enable IC3 interrupts
-	IC3CON = 0x00000206;	// Disable and reset the IC Module, disable interrupt generation and allow SFR modifications;
+	/** IC4/RD11 **/
+	IPC4SET = 0x00001000;	// Set priority level = 4
+	IPC4SET = 0x00000100;	// Set subpriority level = 1
+	IFS0CLR = 0x00020000;	// Clear IC4 interrupt flag
+	IEC0SET = 0x00020000;	// Enable IC4 interrupts
+	IC4CON = 0x00000206;	// Disable and reset the IC Module, disable interrupt generation and allow SFR modifications;
 							// simple Capture Event mode, capture rising edge first, count source is 16-bit Timer3
 							// interrupt on every capture event
-	/** Start **/
-	IC1CONSET = 0x8000;		// start Input Capture 1 (IC1)
-	IC3CONSET = 0x8000;		// start Input Capture 3 (IC3)
 }
 
 /** Get distance **/
-#pragma interrupt IC1_ISR ipl4 vector 5
-void IC1_ISR(void) {
-	if (!IC1begin) TMR3 = 0;
-	else{
-		distance1 = 0.0;
-		unsigned int time = IC1BUF;
-		distance1 = time * 0.00017;
+#pragma interrupt IC2_ISR ipl4 vector 9
+void IC2_ISR(void) {
+	if (Ultrasonic1_ECHO){
+		TMR3 = 0x0;
+		unsigned int time = IC2BUF;
 	}
-	IC1begin  = 1 - IC1begin;
-	IFS0CLR = 0x00000020;
+	else{
+		unsigned int time = IC2BUF;
+		distance1 = time * 0.00017;
+		IC2CONCLR = 0x8000;		// stop Input Capture 2 (IC2)
+	}
+	IFS0CLR = 0x00000200;
 }
 
-#pragma interrupt IC3_ISR ipl4 vector 13
-void IC3_ISR(void) {
-	if (!IC3begin) TMR3 = 0;
-	else{
-		distance2 = 0.0;
-		unsigned int time = IC3BUF;
-		distance2 = time * 0.00017;
+#pragma interrupt IC4_ISR ipl4 vector 17
+void IC4_ISR(void) {
+	if (Ultrasonic2_ECHO){
+		TMR3 = 0x0;
+		unsigned int time = IC4BUF;
 	}
-	IC3begin  = 1 - IC3begin;
-	IFS0CLR = 0x00002000;
+	else{
+		unsigned int time = IC4BUF;
+		distance2 = time * 0.00017;
+		IC4CONCLR = 0x8000;		// stop Input Capture 2 (IC2)
+	}
+	IFS0CLR = 0x00020000;
+}
+
+BOOL User_IsNear(){
+	return (distance1 < 0.6 || distance2 < 0.6);
+}
+
+BOOL User_GoDetector1(){
+	return (User_IsNear() && (distance1 < distance2));
 }
 
 int main() {
